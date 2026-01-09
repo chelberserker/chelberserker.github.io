915a9ae584d80297a667b16dff0e501b6dff2fdf
chelberserker <bersenjow@gmail.com>
2026-01-09 23:19:26 +0000

    Deploying to gh-pages from @ chelberserker/chelberserker.github.io@a1e8de341b8943987cde30148ed06ef302721399 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -96,8 +96,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-10-23-Langmuir-humidity-induced/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
+            },},{id: "projects-surf-python-package",
+          title: 'SURF python package',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
